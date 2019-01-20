f9219aab183f71a7e2f79d388daec4e6ddccf51c
Juan Francisco Lopez <jualo@MacBook-Pro-de-Juan.local>
2019-01-20 17:36:54 +0100

    Fruta component

--- Curso/src/app/app.module.ts
+++ Curso/src/app/app.module.ts
@@ -3,10 +3,11 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import {FrutaComponent} from './fruta/fruta.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FrutaComponent
   ],
   imports: [
     BrowserModule,
